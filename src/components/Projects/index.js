7bb16c4d83903790b778664a763df8cff6acade5
kokiebisu <kenichikona@gmail.com>
2019-08-28 20:00:01 -0700

    fixed bugs

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import './projects.scss';
 
-import card1 from '../../assets/img/card.jpg';
+import ratemycourse from '../../assets/img/ratemycourse.png';
+import card2 from '../../assets/img/card.jpg';
 
 export default () => (
   <div className="projects">
     <h2 className="projects__title">Projects</h2>
     <div className="projects__cards">
       <div className="projects__card projects__card--1">
-        <img src={card1} alt="" />
-        <h2>ratemycourse</h2>
-        <p>Evalute your course. Don't waste time.</p>
-        <p>2019/08/27</p>
+        <a class="anchor" href="">
+          <img src={ratemycourse} alt="" />
+          <h2>ratemycourse</h2>
+          <p>Evalute your course. Don't waste time.</p>
+          <p>2019/08/27</p>
+        </a>
       </div>
       <div className="projects__card projects__card--2">
-        <img src={card1} alt="" />
+        <img src={card2} alt="" />
         <h2>Coming soon...</h2>
         <p>Almost there!</p>
       </div>
       <div className="projects__card projects__card--3">
-        <img src={card1} alt="" />
+        <img src={card2} alt="" />
         <h2>Coming soon...</h2>
         <p>Almost there!</p>
       </div>
