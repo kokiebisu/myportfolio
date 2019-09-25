3b52c00f78967444fd5c46d96aa4bccdc79c8fb4
kokiebisu <kenichikona@gmail.com>
2019-09-25 15:00:34 -0700

    fixed height of sidebar component

--- src/template/index.js
+++ src/template/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import Sidebar from '../components/Sidebar';
 import Bar from '../components/Bar';
+import './template.scss';
 
 export default props => (
   <Fragment>
